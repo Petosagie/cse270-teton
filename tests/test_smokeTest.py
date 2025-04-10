8111eeec26c16bbeee771b23a18588dbe01b5799
Osagie <petosagie@gmail.com>
2025-04-10 22:56:43 +0100

    update

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -9,11 +9,16 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
-    self.vars = {}
+    # self.driver = webdriver.Firefox()
+    # self.vars = {}   
+      options = Options()
+      options.headless = True  # Essential for GitHub Actions!
+      self.driver = webdriver.Firefox(options=options)
+      self.vars = {}
   
   def teardown_method(self, method):
     self.driver.quit()
